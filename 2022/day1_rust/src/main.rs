6c1cca5936c90ede9bde82f6dbe8191ac984aaf4
Jesperdn <jesperdahl@hotmail.no>
2022-12-02 17:07:25 +0100

    day 2 rust

--- 2022/day1_rust/src/main.rs
+++ 2022/day1_rust/src/main.rs
@@ -1,8 +1,47 @@
-use utils::read_lines
+use utils::read_lines;
+use std::collections::BinaryHeap;
 
+fn part_one(file: &str) -> i32 {
+    let mut highest: i32 = 0;
+    let mut calories: i32 = 0;
+    if let Ok(lines) = read_lines(file) {
+        for line in lines {
+            if let Ok(ln) = line {
+                if ln.eq("") {
+                    highest = if calories>highest {calories} else {highest};
+                    calories = 0;
+                } else {
+                    calories+=ln.parse::<i32>().unwrap();
+                }
+            }
+        }
+    }
+    return highest
+}
+
+fn part_two(file: &str) -> i32 {
+    let mut elves = BinaryHeap::new();
+    let mut calories: i32 = 0;
+    if let Ok(lines) = read_lines(file) {
+        for line in lines {
+            if let Ok(ln) = line {
+                if ln.eq("") {
+                    elves.push(calories);
+                    calories = 0;
+                } else {
+                    calories+=ln.parse::<i32>().unwrap();
+                }
+            }
+        }
+    }
+    let mut top_three = 0;
+    for _ in 0..3 {
+        top_three += elves.pop().unwrap();
+    }
+    return top_three;
+}
 
 fn main() {
-    if let Ok(lines) = read_lines("test") {
-        
-    }
+    println!("{}", part_one("input"));
+    println!("{}", part_two("input"));
 }
